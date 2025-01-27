01bafa2ac7f1f7e2e3b9458d9a6c478232178ff4
Igoru <igorpie1705@gmail.com>
2025-01-27 20:25:23 +0100

    przebudowano projekt na nowo

--- sklepjs/src/api/auth/clientAuth.js
+++
@@ -1,9 +0,0 @@
-const apiClient = axios.create({
-  baseURL: "http://localhost:3001/api/",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export default apiClient;
