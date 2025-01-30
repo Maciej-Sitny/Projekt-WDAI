ed0af6c12994d5dc4aadf5bd9433db150fe563ce
Maciej Sitny <maciejsitny@gmail.com>
2025-01-30 07:13:51 +0100

    Dodano blobsy

--- sklep/src/pages/AllProducts.jsx
+++ sklep/src/pages/AllProducts.jsx
@@ -25,51 +25,75 @@ const AllProducts = () => {
   );
 
   return (
-    <div className="container my-4">
-      <h1 className="text-center mb-4">Wszystkie produkty</h1>
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="Wyszukaj przedmiot..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="form-control"
-        />
-      </div>
-      <div className="row">
-        {filteredProducts.map((product) => (
-          <div key={product.id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
-            <div className="card h-100">
-              <img
-                src={product.image}
-                alt={product.title}
-                className="card-img-top"
-                style={{ height: "300px", objectFit: "contain" }}
+      <div className="container my-4">
+          <img src={"src/images/home/blob.svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (1).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "60%", left: "10%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (2).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "30%", left: "80%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (3).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "10%", left: "60%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (5).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "20%", left: "30%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (6).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "70%", left: "70%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (7).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "50%", left: "55%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (8).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "5%", left: "35%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (9).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "90%", left: "35%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (10).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "75%", left: "86%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (11).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "55%", left: "30%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (12).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "43%", left: "15%", height: "100px", width: "100px"}}/>
+          <h1 className="text-center mb-4 position-relative">Wszystkie produkty</h1>
+          <div className="mb-4 position-relative">
+              <input
+                  type="text"
+                  placeholder="Wyszukaj przedmiot..."
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                  className="form-control"
               />
-              <div className="card-body">
-                <h5 className="card-title">{product.title}</h5>
-                <p
-                  className="card-text text-truncate"
-                  style={{ maxHeight: "50px" }}
-                ></p>
-              </div>
-              <div className="card-footer flex-column d-flex align-items-center justify-content-center">
-                <p className="text-muted mb-1">
-                  Cena: ${product.price.toFixed(2)}
-                </p>
-                <Link
-                  to={`/products/${product.id}`}
-                  className="btn btn-warning  mb-2"
-                >
-                  Zobacz opis
-                </Link>
-                <AddToCart product={{ id: product.id, quantity: 1 }} />
-              </div>
-            </div>
           </div>
-        ))}
+          <div className="row">
+              {filteredProducts.map((product) => (
+                  <div key={product.id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
+                      <div className="card h-100">
+                          <img
+                              src={product.image}
+                              alt={product.title}
+                              className="card-img-top"
+                              style={{height: "300px", objectFit: "contain"}}
+                          />
+                          <div className="card-body">
+                              <h5 className="card-title">{product.title}</h5>
+                              <p
+                                  className="card-text text-truncate"
+                                  style={{maxHeight: "50px"}}
+                              ></p>
+                          </div>
+                          <div className="card-footer flex-column d-flex align-items-center justify-content-center">
+                              <p className="text-muted mb-1">
+                                  Cena: ${product.price.toFixed(2)}
+                              </p>
+                              <Link
+                                  to={`/products/${product.id}`}
+                                  className="btn btn-warning  mb-2"
+                              >
+                                  Zobacz opis
+                              </Link>
+                              <AddToCart product={{id: product.id, quantity: 1}}/>
+                          </div>
+                      </div>
+                  </div>
+              ))}
+          </div>
       </div>
-    </div>
   );
 };
 
--- sklep/src/pages/Home.jsx
+++ sklep/src/pages/Home.jsx
@@ -33,7 +33,31 @@ const Home = () => {
     console.log(products)
   return (
       <div className={"container-fluid h-100 "}>
-          <div className={"container-fluid bg-warning  "} style={{marginBottom: "5%", marginTop: "10%"}}>
+          <img src={"src/images/home/blob.svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (1).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "60%", left: "10%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (2).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "30%", left: "80%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (3).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "10%", left: "60%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (5).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "20%", left: "30%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (6).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "70%", left: "70%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (7).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "50%", left: "55%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (8).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "5%", left: "35%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (9).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "90%", left: "35%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (10).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "75%", left: "86%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (11).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "55%", left: "30%", height: "100px", width: "100px"}}/>
+          <img src={"src/images/home/blob (12).svg"} alt={"blob"} className={"position-fixed z-0"}
+               style={{top: "43%", left: "15%", height: "100px", width: "100px"}}/>
+          <div className={"container-fluid bg-warning  position-relative  z-1"} style={{marginBottom: "5%", marginTop: "10%"}}>
               <div className="row w-100 ">
                   <div className="col-6 d-flex justify-content-center align-items-center">
                       <div className="d-flex flex-column justify-content-center align-items-center">
@@ -43,7 +67,7 @@ const Home = () => {
                   </div>
                   <div className="col-6">
                       {products && products.length > 0 && (
-                          <img src={products[0].image} alt={products[0].title} className="img-fluid "
+                          <img src={"src/images/home/noback.png"} alt={products[0].title} className="img-fluid "
                                style={{width: '50%'}}/>
                       )}
 
@@ -53,7 +77,7 @@ const Home = () => {
           </div>
           <img src={"src/images/home/arrowDown.gif"} alt="Loading..."
                style={{width: "100px", height: "100px"}}/>
-          <div className="container mt-4">
+          <div className="container mt-4 position-relative  z-1">
               <div className="row">
                   <div className="col-6  d-flex justify-content-center align-items-center ">
                       <div className="d-flex flex-column justify-content-center align-items-center">
@@ -102,10 +126,60 @@ const Home = () => {
                       </div>
                   </div>
               </div>
+              <div className="row">
+                  <div className="col-6  d-flex justify-content-center align-items-center ">
+                      <div className="d-flex flex-column justify-content-center align-items-center">
+                          <h1>Kolekcja ubrań damskich</h1>
+                          <p>Odkryj naszą kolekcję ubrań damskich, która łączy w sobie elegancję, komfort i najnowsze
+                              trendy modowe</p>
+                          <Link
+                              to={`/category/women's clothing`}
+                              className="btn btn-warning mt-3"
+                          >
+                              Zobacz produkty
+                          </Link>
+                      </div>
+                  </div>
+                  <div className="col-6 d-flex justify-content-center align-items-center p-0">
+
+                      <img
+                          src={"src/images/home/shutterstock_642322564.webp"}
+                          alt={"ubrania damskie"}
+                          className="img-fluid"
+                          style={{objectFit: "cover"}}
+                      />
+
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col-6 d-flex justify-content-center align-items-center p-0">
+                      {products && products.length > 0 && (
+                          <img
+                              src={"src/images/home/152e502c1f16c117f5bb5e34c6da2dcdc0bfe3caf48131cbedd3a3ed2f0060ea._SX1080_FMjpg_.jpg"}
+                              alt={"elektronika"}
+                              className="img-fluid"
+                              style={{backgroundSize: "cover"}}
+                          />
+                      )}
+                  </div>
+                  <div className="col-6 d-flex justify-content-center align-items-center ">
+                      <div className="d-flex flex-column justify-content-center align-items-center">
+                          <h1>Elektronika</h1>
+                          <p>Poznaj naszą szeroką ofertę elektroniki, która obejmuje najnowsze urządzenia i akcesoria,
+                              aby ułatwić Ci codzienne życie i zapewnić rozrywkę na najwyższym poziomie.</p>
+                          <Link
+                              to={`/category/electronics`}
+                              className="btn btn-warning mt-3"
+                          >
+                              Zobacz produkty
+                          </Link>
+                      </div>
+                  </div>
+              </div>
           </div>
           {/*<img src={"src/images/home/arrowDown.gif"} className={"bottom-center-gif"}*/}
           {/*     style={{height: "80px", width: "80px"}} alt="Loading..."/>*/}
-          <div className="container mt-5 bg-warning ">
+          <div className="container mt-5 bg-warning position-relative  z-1">
               <div className="row">
                   <div className="col-9 d-flex flex-column align-items-center p-5">
                       <h1 style={{fontSize: "10vw"}} className="fs-4">Zarejestruj się, by dostawać nasz newsletter</h1>
@@ -117,9 +191,17 @@ const Home = () => {
                       </Link>
                   </div>
               </div>
+
           </div>
-          <footer className="text-center mt-4" style={{display:"flex", justifyContent:"center", alignItems:"center", backgroundColor:"#10375C", height:"50px"}}>
-              <p style={{color:"#F4F6FF"}}>Autorzy: Igor Piesik & Maciej Sitny</p>
+
+          <footer className="text-center mt-4 position-relative" style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#10375C",
+              height: "50px"
+          }}>
+              <p style={{color: "#F4F6FF"}}>Autorzy: Igor Piesik & Maciej Sitny</p>
           </footer>
       </div>)
 
